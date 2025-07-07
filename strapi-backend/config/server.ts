a251d58c00a58e0a571d26762c376f83f209c38d
romanglitch <tagshowpictures@gmail.com>
2025-07-07 17:23:02 +0300

    Update server

--- strapi-backend/config/server.ts
+++ strapi-backend/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 6002),
-  url: 'https://iddit.glitchweb.ru/api',
+  url: 'https://iddit.glitchweb.ru/cms',
   app: {
     keys: env.array('APP_KEYS'),
   },
